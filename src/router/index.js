9df897a190f25c970defaa44e6484b6d6d5dcaf2
Cooper Schahinger <cooperschahinger@gmail.com>
2022-05-17 11:02:08 +0930

    fixed router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
-import router from "vue-router";
+import { createWebHistory, createRouter } from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 
 const routes = [
   {
@@ -10,16 +11,12 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: About,
   },
 ];
 
-const router = new CreateNewRouter({
-  mode: "history",
+const router = createRouter({
+  history: createWebHistory(),
   routes,
 });
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <div class="title-container">
+      <h1>Cooper Schahinger</h1>
+      <h4>Undergraduate Software Engineer</h4>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: denter;
+}
+</style>
 
 <script>
 // @ is an alias to /src
